440ffd528e1daf2fab56b0e2f0642e3b9f2c280b
omranali2111 <aljabri1234@hotmail.com>
2023-09-13 11:05:05 +0400

    adding the classes with needed properties

---
+++ BankSysADO/Accounts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSysADO
+{
+    internal class Accounts
+    {
+        {
+        public int AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public decimal Balance { get; set; }
+
+        // Foreign key property
+        public int UserId { get; set; }
+
+        // Navigation property for the User
+        public Users User { get; set; }
+    }
+}
+}
---
+++ BankSysADO/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSysADO
+{
+    internal class Users
+    {
+       
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
+
